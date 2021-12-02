9bcec5a1c8e16143df436d36c61ccee9749fe3b7
Maciej Przybylski <m-przybylski@hotmail.com>
2021-12-02 17:41:56 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,33 @@
+import { BitGo } from "bitgo";
+import { config } from 'dotenv'
+import minimist from 'minimist'
+
+config()
+
+const token = process.env.TOKEN
+const walletId = process.env.WALLET_ID
+
+const isTest = true
+
+const bitgo = new BitGo({
+  accessToken: token,
+  env: "test",
+  userAgent: "fake-something",
+});
+var argv = minimist(process.argv.slice(2));
+const txId = argv['tx']
+console.log('transaction to bump:', txId)
+
+async function run() {
+  const coin = isTest ? 'tbtc' : 'btc'
+  const wallet = await bitgo.coin(coin).wallets().get({ id: walletId })
+  try {
+    const result = await wallet.accelerateTransaction({ cpfpTxIds: [txId], cpfpFeeRate: 200, maxFee: 6000 })
+    console.log({result})
+  } catch (ex) {
+    console.error({ex})
+  }
+}
+
+
+run()
